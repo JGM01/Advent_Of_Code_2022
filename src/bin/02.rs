e59e49610c7d93c30c84259ef0a089aa640e6309
JGM01 <jacobgermanamccray@gmail.com>
2022-12-02 00:31:47 -0500

    removed  tests

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -94,20 +94,3 @@ fn main() {
     advent_of_code::solve!(1, part_one, input);
     advent_of_code::solve!(2, part_two, input);
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part_one() {
-        let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_one(&input), None);
-    }
-
-    #[test]
-    fn test_part_two() {
-        let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
-    }
-}
